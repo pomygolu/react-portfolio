5a983cbedc5a9cfb1c66f70ceb459a04f354cd83
ROHIT DALMIA <rohitdalmia@ROHITs-MacBook-Air.local>
2024-10-22 02:32:55 +0530

    Changing pictures

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import {HERO_CONTENT} from "../constants";
-import profilePic from "../assets/kevinRushProfile.png";
+import profilePic from "../assets/RohitProfile.png";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
